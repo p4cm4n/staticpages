8077685fd1b263630594e33e5f325bfceae12294
Ti53 <soporte@ti53.es>
2012-11-20 12:33:54 +0100

    default

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Staticpages::Application.config.secret_token = '160099b610c136bd650d0739d54a1f7dc3d539aacf87e0dd0e7c346f7f226342d363d6e5337aa0ca64bc3f580bda19fc630deb4cc70dd85b2b1a7be00ee9e3e8'
